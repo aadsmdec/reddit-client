e868e89fd4e33227029dbb8df89bbad49dbb344c
Afonso <noreply>
2013-12-10 22:59:56 +0000

    Update component /ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -13,5 +13,16 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function Main() {
             this.super();
         }
-    }
+    },
+    
+    subs: {
+        value: [
+            { display_name: "HTML", url: "/r/html5" },
+            { display_name: "Programming", url: "/r/programming" },
+            { display_name: "Coding", url: "/r/coding" },
+            { display_name: "Comp Sci", url: "/r/compsci" },
+            { display_name: "Web Dev", url: "/r/webdev" },
+            { display_name: "Startups", url: "/r/startups" }
+        ]
+      }
 });
